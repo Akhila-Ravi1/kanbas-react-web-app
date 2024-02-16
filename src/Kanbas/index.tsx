edbba2a235a266c63c1e74b3c83eb65d6b7ce7b4
Akhila-Ravi1 <akhilar028@gmail.com>
2024-02-16 16:24:48 -0500

    Code for Kanbas Dashboard

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,15 +1,23 @@
-import { Link } from "react-router-dom";
-import Nav from "../Nav";
+import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function Kanbas() {
    return (
       <div className="d-flex">
          <KanbasNavigation />
-         <div style={{flexGrow: 1}}>
-            <h1>Account</h1>
+         <div style={{ flexGrow: 1 }}>
+            {/* <h1>Account</h1>
             <h1>Dashboard</h1>
-            <h1>Courses</h1>
+            <h1>Courses</h1> */}
+
+            <Routes>
+               <Route path="/" element={<Navigate to="Dashboard" />} />
+               <Route path="Account" element={<h1>Account</h1>} />
+               <Route path="Dashboard" element={<Dashboard />} />
+               <Route path="Courses/*" element={<h1>Courses</h1>} />
+            </Routes>
+
          </div>
       </div>
    )
