06c0b50538f9eaa3bde4dced179ef270b9c47ea2
Akhila-Ravi1 <akhilar028@gmail.com>
2024-03-05 14:20:02 -0500

    Code for lab till 2.4.3

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -3,6 +3,14 @@ import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
 import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
 
 function Assignment4() {
     function sayHello() {
@@ -12,10 +20,19 @@ function Assignment4() {
     return (
         <>
             <h1>Assignment 4</h1>
+            <ReduxExamples />
+
             <ClickEvent />
             <PassingDataOnEvent />
             <PassingFunctions theFunction={sayHello} />
             <EventObject />
+            <Counter />
+            <BooleanStateVariables />
+            <StringStateVariables />
+            <DateStateVariable />
+            <ObjectStateVariable />
+            <ArrayStateVariable />
+            <ParentStateComponent />
         </>
     );
 };
